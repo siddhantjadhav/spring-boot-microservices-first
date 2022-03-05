61c7723f6d75cf1f3abcec5e87b5bd8e42f1e0f5
siddhantjadhav <jadhav.siddhant11@gmail.com>
2022-03-05 21:49:34 +0530

    rating service base implementation

--- ratings-data-service/src/main/java/com/example/ratingsdataservice/controller/RatingsDataController.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/controller/RatingsDataController.java
@@ -1,10 +1,16 @@
 package com.example.ratingsdataservice.controller;
 
+import com.example.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsDataController {
-    
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
 }
